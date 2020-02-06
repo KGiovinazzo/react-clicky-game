7a59b007434e566c65c691fe4a0338da267286b9
Kaitlyn Giovinazzo <kgiovinazzo42@gmail.com>
2020-02-05 19:49:46 -0500

    Updates

---
+++ precache-manifest.9bc39a10860605c1fa5cf0f943c14243.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130b4f126533f1bb036a548d25d600e8",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "3932cfc29cb1404c0b70",
+    "url": "/react-clicky-game/static/css/main.ceecf336.chunk.css"
+  },
+  {
+    "revision": "92e68c3a1cfe74c9d40e",
+    "url": "/react-clicky-game/static/js/2.a1228723.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-clicky-game/static/js/2.a1228723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3932cfc29cb1404c0b70",
+    "url": "/react-clicky-game/static/js/main.9dbb394f.chunk.js"
+  },
+  {
+    "revision": "cd7e32d7c1314b32f128",
+    "url": "/react-clicky-game/static/js/runtime-main.7156d541.js"
+  }
+]);
